3c4954d2a3128b5221bbb88611ab0952ef7f83d8
Pietro Bottega <pietro.bottega000@gmail.com>
2025-07-31 20:34:49 -0300

    debugging app

--- scr/app.py
+++ scr/app.py
@@ -3,9 +3,16 @@ import pandas as pd
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-fipe_features_PCA = pd.read_csv("../data/output/fipe_features_PCA.csv")
-fipe_features = pd.read_csv("../data/output/fipe_features.csv")
-fipe_data = pd.read_csv("../data/output/fipe_data.csv")
+dir = path.Path(__file__).abspath()
+sys.path.append(dir.parent.parent)
+
+fipe_features_PCA_path = "../data/output/fipe_features_PCA.csv"
+fipe_features_path = "../data/output/fipe_features.csv"
+fipe_data_path = "../data/output/fipe_data.csv"
+
+fipe_features_PCA = pd.read_csv(fipe_features_PCA_path)
+fipe_features = pd.read_csv(fipe_features_path)
+fipe_data = pd.read_csv(fipe_data_path)
 
 no_models = fipe_features.shape[0] -1
 
